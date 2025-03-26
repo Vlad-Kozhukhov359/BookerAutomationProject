6f66a3a1607d507e43425b64288b693a049c797b
Vlad-Kozhukhov359 <kozhuhof359@gmail.com>
2025-03-26 21:01:46 +0300

    Код показанный на уроке Filter в Rest Assured

--- src/main/java/core/client/APIClient.java
+++ src/main/java/core/client/APIClient.java
@@ -3,7 +3,11 @@ package core.client;
 
 import core.settings.ApiEndpoints;
 import io.restassured.RestAssured;
+import io.restassured.filter.Filter;
+import io.restassured.filter.FilterContext;
 import io.restassured.response.Response;
+import io.restassured.specification.FilterableRequestSpecification;
+import io.restassured.specification.FilterableResponseSpecification;
 import io.restassured.specification.RequestSpecification;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +22,7 @@ public class APIClient {
     private static final Logger logger = LoggerFactory.getLogger(APIClient.class);
 
     private final String baseUrl;
+    private String token;
 
     public APIClient() {
         this.baseUrl = determineBaseUrl();
@@ -50,7 +55,39 @@ public class APIClient {
         return RestAssured.given()
                 .baseUri(baseUrl)
                 .header("Content-Type", "application/json")
-                .header("Accept", "application/json");
+                .header("Accept", "application/json")
+                .filter(addAuthTokenFilter());
+    }
+
+    // Метод для получения токена
+    public void createToken(String username, String password) {
+        // Формирование JSON тела для запроса
+        String requestBody = String.format("{ \"username\": \"%s\",\"password\": \"%s\" }", username, password);
+        // Отправка POST-запроса на эндпоинт для аутентификации и получение токена
+        Response response = getRequestSpec()
+                .body(requestBody) // Устанавливаем тело запроса
+                .when()
+                .post(ApiEndpoints.AUTH.getPath()) // POST-запрос на эндпоинт аутентификации
+                .then()
+                .log().all()
+                .statusCode(200) // Проверяем, что статус ответа 200 (ОК)
+                .extract()
+                .response();
+        // Извлечение токена из ответа и сохранение в переменной
+        token = response.jsonPath().getString("token");
+    }
+
+    // Фильтр addAuthTokenFilter добавляет токен в заголовок Cookie каждого запроса. Этот
+    //фильтр срабатывает перед отправкой запроса.
+
+    private Filter addAuthTokenFilter() {
+        return (FilterableRequestSpecification requestSpec,
+                FilterableResponseSpecification responseSpec, FilterContext ctx) -> {
+            if (token != null) {
+                requestSpec.header("Cookie", "token=" + token);
+            }
+            return ctx.next(requestSpec, responseSpec); // Продолжает выполнениезапроса
+        };
     }
 
     // GET запрос на эндпоинт / ping
@@ -92,6 +129,24 @@ public class APIClient {
                 .extract()
                 .response();
         logger.info("Received response for /booking/{}: status code {}", id, response.getStatusCode());
+        return response;
+    }
+
+    // DELETE запрос на эндпоинт /booking
+    public Response deleteBooking(int bookingId) {
+        logger.info("Starting DELETE request for booking ID: {}", bookingId); // Логируем ID бронирования
+
+        Response response = getRequestSpec()
+                .pathParam("id", bookingId) // Указываем path parameter для ID
+                .when()
+                .delete(ApiEndpoints.BOOKING.getPath() + "/{id}") // Используем метод, в который можно подставлять id
+                .then()
+                .log().all() // Логируем весь запрос и ответ
+                .statusCode(201)
+                .extract()
+                .response();
+
+        logger.info("Response status code: {}", response.getStatusCode()); // Логируем статус ответа
         return response;
     }
 }
